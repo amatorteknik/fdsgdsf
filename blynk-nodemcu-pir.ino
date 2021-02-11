54f664944c697811714274326d4bdbb5e6c1d120
amatorteknik <78912430+amatorteknik@users.noreply.github.com>
2021-02-11 16:01:56 +0300

    Add files via upload

---
+++ blynk-nodemcu-pir.ino
@@ -0,0 +1,37 @@
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+int pirPin = 14;
+int relayPin = 12;
+int pirValue;
+
+char auth[] = "d16a1dbf5a8640f688017bfd8b969aac";
+char ssid[] = "ASUS";
+char pass[] = "asus123asus";
+
+void setup()
+{
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+  pinMode(pirPin, INPUT);
+  pinMode(relayPin, OUTPUT);
+}
+
+void pirSensor()
+{
+  pirValue = digitalRead(pirPin);
+  Blynk.virtualWrite(V0, pirValue);
+}
+
+void loop()
+{
+  Blynk.run();
+  pirSensor();
+  if (pirValue == HIGH)
+  {
+    digitalWrite(relayPin, LOW);
+  }
+  
+  else
+  digitalWrite(relayPin, HIGH); 
+}
